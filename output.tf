62e26e96ccc3331e69a2a5a0fba314772a5552ec
Akin Kuyga <akinkuyga35@gmail.com>
2023-04-12 16:47:05 -0400

    sh and tf files are added

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "URL" {
+  value = "http://${azurerm_public_ip.pip.ip_address}:8080"
+}
+
+output "SSH_Command" {
+  value = "ssh -i ${var.ssh_private_key_path}${var.ssh_key_name}.pem ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
+}
+
+output "Jenkins_Password_Retrieval" {
+  value = "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "prefix" {
+  default = "jenkins"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "admin_username" {
+  default = "clouduser"
+}
+
+variable "ssh_key_rg" {
+  default = "akin"
+}
+
+variable "ssh_key_name" {
+  default = "akinssh1"
+}
+
+variable "ssh_private_key_path" {
+  default = "~/Desktop/ssh/"
+}
